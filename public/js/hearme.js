c507197be78b3fc72fd3c0a8d5440186747be489
Marcus Ljungblad <marcus@ljungblad.nu>
2014-04-01 15:29:44 +0200

    Refactored to avoid use of global scope. Fixes #3

--- public/js/hearme.js
+++ public/js/hearme.js
@@ -1,38 +1,41 @@
-(function(){
+var Locator = function(lat, lon) {
   'use strict';
 
-  // global variables
-  var defaultLatLong = new google.maps.LatLng(55.604981,13.003822); // Malmo, Sweden
-  var map;
+  var defaultLatLong = new google.maps.LatLng(lat, lon);
+  var map = new google.maps.Map(document.getElementById('map-canvas'), { zoom: 11 });
 
   // load map and set to current position
-  function initialize() {
-    
-    // default
-    map = new google.maps.Map(document.getElementById('map-canvas'), { zoom: 11 });
-
-    // lookup location on map 
+  this.run = function() {
+    // find our position 
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(codeLatLng,
+      navigator.geolocation.getCurrentPosition(function(pos) {
+        codeLatLng.call(this, pos);
+      },
       function() {
-        handleNoGeolocation(true);
+        handleNoGeolocation.call(this, true);
       });
     }
-  }
 
-  function codeLatLng(pos) {
+    // make sure center is always at the center
+    $(window).resize(function() {
+      map.setCenter(defaultLatLong);
+    });
+  };
+
+  var codeLatLng = function(pos) {
     var marker;
-    var geocoder;
+    var geocoder = new google.maps.Geocoder();
     var infowindow = new google.maps.InfoWindow();
+    var lat = pos.coords.latitude;
+    var lon = pos.coords.longitude;
+    var latlng = new google.maps.LatLng(lat, lon);
 
-    geocoder = new google.maps.Geocoder();
-    var latlng = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
     defaultLatLong = latlng; // update default
 
     geocoder.geocode({'latLng': latlng}, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
         if (results[1]) {
-          setFormVariables(results[1], pos.coords.latitude, pos.coords.longitude);
+          setFormVariables.call(this, results[1], lat, lon);
           map.setZoom(16);
           map.setCenter(latlng);
           marker = new google.maps.Marker({
@@ -42,34 +45,33 @@
           infowindow.setContent(results[1].formatted_address);
           infowindow.open(map, marker);
         } else {
-          handleNoGeolocation(false);
+          handleNoGeolocation.call(this, false);
         }
       } else {
         alert('Geocoder failed due to: ' + status);
       }
     });
-  }
+  };
 
-  function handleNoGeolocation(errorFlag) {
+  var handleNoGeolocation = function(errorFlag) {
     var mapOptions = {
       zoom: 11,
       center: defaultLatLong,
       mapTypeId: 'roadmap'
     };
-    map.setCenter(mapOptions.center);
-  }
 
-  function setFormVariables(result, latitude, longitude) {
+    map.setCenter(mapOptions.center);
+  };
+
+  var setFormVariables = function(result, latitude, longitude) {
     $('#location_name_tag').text('You are at ' + result.formatted_address);
     $('#location_latitude_field').val(latitude);
     $('#location_longitude_field').val(longitude);
-  }
+  };
 
-  // global event handlers
-  $(window).resize(function() {
-    map.setCenter(defaultLatLong);
-  });
+  return this;
+};
 
-  google.maps.event.addDomListener(window, 'load', initialize);
-
-})(); 
+// Run locator on document load
+var locator = new Locator(55.604981, 13.003822); // malmo, sweden
+google.maps.event.addDomListener(window, 'load', locator.run);
