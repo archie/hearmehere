a3bdb3b9575d8f437c9ee0d3fdbaf86aa68175c6
Marcus Ljungblad <marcus@ljungblad.nu>
2014-04-01 11:25:56 +0200

    Activate use strict on all js code. Fixes #1

--- public/js/hearme.js
+++ public/js/hearme.js
@@ -1,70 +1,75 @@
-// global variables
-var defaultLatLong = new google.maps.LatLng(55.604981,13.003822); // Malmo, Sweden
-var map;
+(function(){
+  'use strict';
 
-// load map and set to current position
-function initialize() {
-  
-  // default
-  map = new google.maps.Map(document.getElementById('map-canvas'), { zoom: 11 });
+  // global variables
+  var defaultLatLong = new google.maps.LatLng(55.604981,13.003822); // Malmo, Sweden
+  var map;
 
-  // lookup location on map 
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(codeLatLng,
-    function() {
-      handleNoGeolocation(true);
+  // load map and set to current position
+  function initialize() {
+    
+    // default
+    map = new google.maps.Map(document.getElementById('map-canvas'), { zoom: 11 });
+
+    // lookup location on map 
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(codeLatLng,
+      function() {
+        handleNoGeolocation(true);
+      });
+    }
+  }
+
+  function codeLatLng(pos) {
+    var marker;
+    var geocoder;
+    var infowindow = new google.maps.InfoWindow();
+
+    geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+    defaultLatLong = latlng; // update default
+
+    geocoder.geocode({'latLng': latlng}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        if (results[1]) {
+          setFormVariables(results[1], pos.coords.latitude, pos.coords.longitude);
+          map.setZoom(16);
+          map.setCenter(latlng);
+          marker = new google.maps.Marker({
+              position: latlng,
+              map: map
+          });
+          infowindow.setContent(results[1].formatted_address);
+          infowindow.open(map, marker);
+        } else {
+          handleNoGeolocation(false);
+        }
+      } else {
+        alert('Geocoder failed due to: ' + status);
+      }
     });
   }
-}
 
-function handleNoGeolocation(errorFlag) {
-  var mapOptions = {
-    zoom: 11,
-    center: defaultLatLong,
-    mapTypeId: 'roadmap'
+  function handleNoGeolocation(errorFlag) {
+    var mapOptions = {
+      zoom: 11,
+      center: defaultLatLong,
+      mapTypeId: 'roadmap'
+    };
+    map.setCenter(mapOptions.center);
   }
-  map.setCenter(mapOptions.center);
-}
 
-function codeLatLng(pos) {
-  var marker;
-  var geocoder;
-  var infowindow = new google.maps.InfoWindow();
+  function setFormVariables(result, latitude, longitude) {
+    $('#location_name_tag').text('You are at ' + result.formatted_address);
+    $('#location_latitude_field').val(latitude);
+    $('#location_longitude_field').val(longitude);
+  }
 
-  geocoder = new google.maps.Geocoder();
-  var latlng = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
-  defaultLatLong = latlng; // update default
-
-  geocoder.geocode({'latLng': latlng}, function(results, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-      if (results[1]) {
-        setFormVariables(results[1], pos.coords.latitude, pos.coords.longitude);
-        map.setZoom(16);
-        map.setCenter(latlng);
-        marker = new google.maps.Marker({
-            position: latlng,
-            map: map
-        });
-        infowindow.setContent(results[1].formatted_address);
-        infowindow.open(map, marker);
-      } else {
-        handleNoGeolocation(false);
-      }
-    } else {
-      alert('Geocoder failed due to: ' + status);
-    }
+  // global event handlers
+  $(window).resize(function() {
+    map.setCenter(defaultLatLong);
   });
-}
 
-function setFormVariables(result, latitude, longitude) {
-  $('#location_name_tag').text('You are at ' + result.formatted_address);
-  $('#location_latitude_field').val(latitude);
-  $('#location_longitude_field').val(longitude);
-}
+  google.maps.event.addDomListener(window, 'load', initialize);
 
-// global event handlers
-$(window).resize(function() {
-  map.setCenter(defaultLatLong);
-});
-
-google.maps.event.addDomListener(window, 'load', initialize);
+})(); 
